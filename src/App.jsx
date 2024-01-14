51f558d13bacae54be6ae92fa98af305d763471e
makimakimek <melodiezgi17@gmail.com>
2024-01-14 23:57:50 +0300

    navigation bar added with html and css

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,23 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className = "page">
+      <div className = "container1">
+        <div className = "navigationContainer">
+          <div className = "navigationLeft">
+            <div>inno</div>
+          </div>
+
+          <div className = "navigationRight">
+            <div className = "link">Anasayfa</div>
+            <div className = "link">Hakkımızda</div>
+            <div className = "link">Hizmetlerimiz</div>
+            <div className = "link">Ürünlerimiz</div>
+            <div className = "link">İletişim</div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
