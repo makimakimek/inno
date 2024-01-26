c473290b27b97c8ac0cfdbf337af2b898e694151
makimakimek <melodiezgi17@gmail.com>
2024-01-27 02:34:08 +0300

    started with the form on the contact page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,6 +3,18 @@ import './App.css'
 import { Link } from "react-router-dom";
 
 function Contact() {
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
     return (
         <>
         <div className = "page">
@@ -29,6 +41,23 @@ function Contact() {
                     </div>
                 </div>
             </div>
+
+            <div className = "pageTitle">
+                İletişim
+            </div>
+
+            <form onSubmit = {handleSubmit}>
+                <label>Enter your name:
+                    <input type = "text" name = "username" value = {inputs.username || ""} onChange={handleChange} />
+                </label>
+      
+                <label>Enter your age:
+                    <input type = "number" name = "age" value={inputs.age || ""} onChange={handleChange} />
+                </label>
+            
+                <input type="submit" />
+            </form>
+
         </div>
         </>
       )
